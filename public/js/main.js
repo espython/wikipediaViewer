33d220fe4edf2c2f1d1539a40f56e154dec36bf6
es <espython85@gmail.com>
2017-05-10 18:43:08 +0200

    get data from wikipedia

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,25 @@
 $(document).ready(function() {
     // Stuff to do as soon as the DOM is ready
-    console.log("I'm here ....");
-    var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search' + searchVar + "&format=json&callback=?";
 
-    $.getJSON(url, function(data) {
+    $('#click').on('click', function() {
+        var input = $('#searchbox').val();
+        console.log(input);
+        var url = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=' + input + '&gsrlimit=10&prop=extracts&exintro&explaintext&exsentences=1&exlimit=max'
 
+        $.ajax({
+            type: "GET",
+            url: url,
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            success: function(data) {
+                console.log(data);
 
-
-
+            },
+            error: function(errorMessage) {
+                alert("Error");
+            }
+        });
     });
+
+
 });
